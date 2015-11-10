f089c01baf7c293a6b3ed434f9f73afa82c66ebd
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2015-11-10 15:35:47 -0500

    draft of binary tree/avl-tree types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
 #[test]
 fn it_works() {
 }
+
+enum BinaryTree<'a, V: 'a, M: 'a> {
+    Branch {
+        metadata: M,
+        value: V,
+        left: &'a BinaryTree<'a, V, M>,
+        right: &'a BinaryTree<'a, V, M>
+    },
+    Leaf {
+        value: V,
+        metadata: M
+    }
+}
+
+type AvlTree<'a, V: 'a> = BinaryTree<'a, V, i32>;
+
+static EXAMPLE_TREE: BinaryTree<'static, i32, i32> = BinaryTree::Branch {
+    metadata: 0,
+    value: 4,
+    left: &BinaryTree::Leaf {
+        metadata: 0,
+        value: 3
+    },
+    right: &BinaryTree::Leaf {
+        metadata: 0,
+        value: 5
+    }
+};

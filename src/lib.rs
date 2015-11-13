b6d1f5ec57178e9a5f5aa74e1b311cad45325fc7
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2015-11-13 04:33:48 -0500

    fully functioning AVL tree!

--- src/lib.rs
+++ src/lib.rs
@@ -114,7 +114,7 @@ type AvlTree<'a, V: 'a> = BinaryTree<V, (i8, i8)>;
 impl <'a, V: Ord+Copy> AvlTree<'a, V> {
     #[allow(non_shorthand_field_patterns)]
     fn insert(&mut self, new_value: V) -> i8 {
-        match *self {
+        let ret = match *self {
             BinaryTree {value, metadata: _, left: None, right: None} => {
                 if new_value > value {
                     *self = BinaryTree {
@@ -149,32 +149,118 @@ impl <'a, V: Ord+Copy> AvlTree<'a, V> {
                 let incr = left.insert(new_value);
                 *left_height += incr;
                 assert!(incr < 2);
-                if *left_height > right_height { incr } else { 0 }
+                if *left_height == right_height + 1 { incr } else { 0 }
             }
             BinaryTree {metadata: (ref mut left_height, right_height), ref mut value, ref mut left, right: _} if new_value < *value => {
                 assert_eq!(0, *left_height);
 
                 *left = Some(Box::new(BinaryTree {value: new_value, metadata: (0, 0), left: None, right: None}));
                 *left_height += 1;
-                if *left_height > right_height { 1 } else { 0 }
+                if *left_height == right_height + 1 { 1 } else { 0 }
             }
             BinaryTree {metadata: (left_height, ref mut right_height), ref mut value, left: _, right: Some(ref mut right)} if new_value > *value => {
                 let incr = right.insert(new_value);
                 *right_height += incr;
                 assert!(incr < 2);
-                if *right_height > left_height { incr } else { 0 }
+                if *right_height == left_height + 1 { incr } else { 0 }
             }
             BinaryTree {metadata: (left_height, ref mut right_height), ref mut value, left: _, right: ref mut right} if new_value > *value => {
                 assert_eq!(0, *right_height);
 
                 *right = Some(Box::new(BinaryTree {value: new_value, metadata: (0, 0), left: None, right: None}));
                 *right_height += 1;
-                if *right_height > left_height { 1 } else { 0 }
+                if *right_height == left_height + 1 { 1 } else { 0 }
             }
             BinaryTree {metadata: _, ref mut value, left: _, right: _} if *value == new_value => {
                 0 // this is a duplicate value, do nothing.
             }
             BinaryTree {metadata: _, value: _, left: _, right: _} => unreachable!()
+        };
+        self.balance();
+        self.fix_metadata();
+        ret
+    }
+
+    fn fix_metadata(&mut self) {
+        match self {
+            &mut BinaryTree {metadata: (left, right), value: _,
+                        left: Some(box BinaryTree {metadata: (left_left, left_right), value: _, left: _, right: _}),
+                        right: Some(box BinaryTree {metadata: (right_left, right_right), value: _, left: _, right: _})}
+            => {
+                self.metadata = (std::cmp::max(left_left, left_right) + 1, std::cmp::max(right_left, right_right) + 1);
+                //println!("{:?} {:?}", self.metadata, (left, right));
+            }
+            &mut BinaryTree {metadata: (left, right), value: _,
+                        left: None,
+                        right: Some(box BinaryTree {metadata: (right_left, right_right), value: _, left: _, right: _})}
+            => {
+                self.metadata = (0, std::cmp::max(right_left, right_right) + 1);
+                //println!("{:?} {:?}", self.metadata, (left, right));
+            }
+            &mut BinaryTree {metadata: (left, right), value: _,
+                        left: Some(box BinaryTree {metadata: (left_left, left_right), value: _, left: _, right: _}),
+                        right: None}
+            => {
+                self.metadata = (std::cmp::max(left_left, left_right) + 1, 0);
+                //println!("{:?} {:?}", self.metadata, (left, right));
+            }
+            &mut BinaryTree {metadata: (left, right), value: _,
+                        left: None,
+                        right: None}
+            => {
+                self.metadata = (0, 0);
+                //println!("{:?} {:?}", self.metadata, (left, right));
+            }
+        }
+    }
+
+    fn rotate_left(&mut self) {
+        let mut tmp = self.clone();
+        let mut right = self.right.clone().expect("trying to rotate right subtree up");
+        tmp.right = right.left.clone();
+        tmp.fix_metadata();
+        right.left = Some(box tmp);
+        *self = *right;
+        self.fix_metadata();
+    }
+
+    fn rotate_right(&mut self) {
+        let mut tmp = self.clone();
+        let mut left = self.left.clone().expect("trying to rotate left subtree up");
+        tmp.left = left.right.clone();
+        tmp.fix_metadata();
+        left.right = Some(box tmp);
+        *self = *left;
+        self.fix_metadata();
+    }
+
+    fn balance(&mut self) {
+        let difference: i8 = self.metadata.0 - self.metadata.1;
+
+        if difference == 2  {
+            match self.left {
+                Some(ref mut left @ box BinaryTree {metadata: _, value: _, left: None, right: Some(_)}) => {
+                        left.rotate_left();
+                }
+                Some(ref mut left @ box BinaryTree {metadata: _, value: _, left: Some(_), right: Some(_)}) if left.metadata.0 - left.metadata.1 < 0 => {
+                        left.rotate_left();
+                }
+                _ => ()
+            }
+            self.rotate_right();
+        } else if difference == -2 {
+            match self.right {
+                Some(ref mut right @ box BinaryTree {metadata: _, value: _, right: None, left: Some(_)}) => {
+                        right.rotate_right();
+                }
+                Some(ref mut right @ box BinaryTree {metadata: _, value: _, right: Some(_), left: Some(_)}) if right.metadata.0 - right.metadata.1 > 0 => {
+                        right.rotate_right();
+                }
+                _ => ()
+            }
+            self.rotate_left();
+        } else if difference < -2 && difference > 2 {
+            unreachable!()
         }
     }
 }

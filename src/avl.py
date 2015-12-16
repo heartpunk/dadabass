3667c4e84c7346dc2a1a393483d993699fd671da
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2015-12-15 20:07:39 -0500

    WIP

--- src/avl.py
+++ src/avl.py
@@ -38,21 +38,9 @@ class AVLTreeNode():
     def left(self):
         return self._left
 
-    @left.setter
-    def left(self, value):
-        self._left = value
-        self._left.parent = self
-        self.left_height = self._left.max_height + 1 if self._left else 0
-
     @property
     def right(self):
         return self._right
-
-    @right.setter
-    def right(self, value):
-        self._right = value
-        self._right.parent = self
-        self.right_height = self._right.max_height + 1 if self._right else 0
 
     def __str__(self, depth=0):
         def helper(child):
@@ -82,8 +70,8 @@ class AVLTreeNode():
         return self.left is None and self.right is None
 
     def become_branch(self):
-        self.left = self.empty_leaf()
-        self.right = self.empty_leaf()
+        self._left = self.empty_leaf()
+        self._right = self.empty_leaf()
 
     @property
     def balance_factor(self) -> int:
@@ -102,43 +90,57 @@ class AVLTreeNode():
 
         self.balance()
 
+    # this could be __getitem__ (same for set_child and __setitem__)
+    def child(self, side):
+        assert side in ("left", "right")
+        return getattr(self, side)
+
+    def set_child(self, side, value):
+        assert side in ("left", "right")
+        setattr(self, "_%s" % side, value)
+        child = self.child(side)
+        child.parent = self
+        setattr(self, "%s_height" % side, child.max_height + 1 if child else 0)
+
+    def height(self, side):
+        assert side in ("left", "right")
+        return getattr(self, "%s_height" % side)
+
     def balance(self):
         # if this assertion fails, the tree is more imbalanced than it ever should be.
         assert(self.balance_factor in (-2,-1,0,1,2))
 
+        side = None
         if self.balance_factor == 2:
-            if (self.left.right and not self.left.left) or \
-               (self.left.right and self.left.left and \
-                # not sure why the following line matters, blindly ported from rust
-                self.left.left_height - self.left.right_height < 0):
-               self.left.rotate("left")
-            self.rotate("right")
+            side = "left"
+            other_side = "right"
         if self.balance_factor == -2:
-            if (self.right.left and not self.right.right) or \
-               (self.right.left and self.right.right and \
+            side = "right"
+            other_side = "left"
+
+        if side:
+            if (self.child(side).child(other_side) and not self.child(side).child(side)) or \
+               (self.child(side).child(other_side) and self.child(side).child(side) and \
                 # not sure why the following line matters, blindly ported from rust
-                self.right.right_height - self.right.left_height < 0):
-               self.right.rotate("right")
-            self.rotate("left")
+                self.child(side).height(side) - self.child(side).height(other_side) < 0):
+               self.child(side).rotate(side)
+            self.rotate(other_side)
 
         if not self.balance_factor in (-1,0,1):
             print(self)
             raise ValueError("the tree is too imbalanced after we attempted to balance it. "
                    "all hope is lost.")
 
-    def rotate(self, direction):
-        assert(direction in ("left", "right"))
+    def rotate(self, rotating_side):
+        assert(rotating_side in ("left", "right"))
+
+        other_side = "left" if rotating_side == "right" else "right"
 
         old_parent = self.parent
 
-        if direction == "left":
-            pivot = self.right
-            self.right = pivot.left
-            pivot.left = self
-        elif direction == "right":
-            pivot = self.left
-            self.left = pivot.right
-            pivot.right = self
+        pivot = self.child(rotating_side)
+        self.set_child(other_side, pivot.child(rotating_side))
+        pivot.set_child(rotating_side, self)
 
         if old_parent is None:
             self.container.root = pivot
